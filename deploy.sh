83e96def3edd1158db4ff57bf2172479aad5cca6
Joseph Ezekiel <theolujay@gmail.com>
2026-05-23 08:28:35 +0100

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 docker build -t ghcr.io/verboheit/quizzo:latest .
-docker push ghcr.io/verboheit/quizzo:latest
+docker pull ghcr.io/verboheit/quizzo:latest
 docker stack deploy quizzo -c stack.yml --detach --prune --resolve-image always --with-registry-auth
