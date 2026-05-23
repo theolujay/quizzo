2348c13188b3496b61afb55c384b4fcee7424e70
Joseph Ezekiel <theolujay@gmail.com>
2026-05-23 08:44:25 +0100

    update deployment scripts to use local image and remove unnecessary pull command

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 git pull
-docker build -t ghcr.io/verboheit/quizzo:latest .
-docker pull ghcr.io/verboheit/quizzo:latest
 docker stack rm quizzo
+docker build -t quizzo:latest .
 docker stack deploy quizzo -c stack.yml --detach --prune --resolve-image always --with-registry-auth
